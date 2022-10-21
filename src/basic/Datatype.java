dfee101559972df5d5611c45bbdae479cca5f492
vighneshDhamale <vighneshdhamale01@gmail.com>
2022-10-21 19:28:40 +0530

    first commit

---
+++ src/basic/Datatype.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class Datatype {
+	public static void main(String[] args) {
+		byte b;
+		b=15;
+		System.out.println(b);
+		
+		short s;
+		s=45;
+		System.out.println(s);
+		
+		int i;
+		i=60;
+		System.out.println(i);
+		
+		long l;
+		l=75;
+		System.out.println(l);
+	}
+
+}
